b8c9c0719231b1734b91fe56d468e764c1fc8e73
odilboooy10 <odilkhon318@gmail.com>
2024-02-23 17:02:58 +0900

    Feat: Burak Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+console.log("EXUCUTED");
+
+import moment from "moment";
+
+const currentTime = moment().format("YYY MM DD");
+console.log(currentTime);
+
+const person: string = "Nick";
+const count: number = 100;
