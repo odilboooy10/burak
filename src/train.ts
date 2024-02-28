0003e86e64e9055135f7d6dd61d2f5d17b322f94
odilboooy10 <odilkhon318@gmail.com>
2024-02-28 18:45:56 +0900

    feat: TASK H

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,17 @@
-console.log("Hello World");
+console.log("TRAIN TASKS!!!");
+
+// H-TASK
+
+function getPositive(arr: number[]) {
+    let positiveNums = ''
+
+    for(let i = 0; i <= arr.length; i++){
+        if(arr[i] > 0) {
+           positiveNums += arr[i]   
+        }
+     }
+
+     return positiveNums
+}
+
+console.log(getPositive([21,-48,211,-10,596]));
