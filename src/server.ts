d6a94f766d819ff157a6e8cf6d3fc11a72e386f1
odilboooy10 <odilkhon318@gmail.com>
2024-03-03 23:43:09 +0900

    feat: develop restaurant login process

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import mongoose from "mongoose";
 import app from "./app";
 mongoose.connect(process.env.MONGO_URL as string, {})
 .then(data => {
-    console.log("MongoDB connection succeed");
+    console
+.log("MongoDB connection succeed");
     const PORT = process.env.PORT ?? 3003;
     app.listen(PORT, function () {
         console.log(`The Server is Running Successfully on port: ${PORT}`);
