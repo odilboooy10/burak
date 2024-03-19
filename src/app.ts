34e8a2a7c3670e4e1ad8b77353e37e815e02b743
odilboooy10 <odilkhon318@gmail.com>
2024-03-19 14:54:57 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,15 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+    uri: String(process.env.MONGO_URL),
+    collection: "sessions",
+  });
+  
 /** 1-Entrance **/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,6 +22,18 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 /** 2-Sessions **/
+app.use(
+    session({
+        secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+  },
+  store: store,
+  resave: true,
+  saveUninitialized: true
+    })
+);
+
 
 /** 3-Views **/
 app.set("views", path.join(__dirname, "views"));
