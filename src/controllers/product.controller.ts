8464c1e132ee6111279596587dda264c4558bf5c
odilboooy10 <odilkhon318@gmail.com>
2024-03-22 14:52:51 +0900

    feat: upload files into server via multer

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import Errors from "../libs/Errors";
 import { T } from "../libs/types/common";
-import ProductService from "../models/Product.service";
+import ProductService from "../models/Product.service";  
 
 const productService = new ProductService();
 
@@ -20,6 +20,7 @@ productController.getAllProducts = async (req: Request, res: Response) => {
 productController.createNewProduct = async (req: Request, res: Response) => {
   try{
     console.log("createNewProduct");
+    res.send ("Done!");
   } catch (err) {
     console.log("Error, createNewProduct:", err);
     if(err instanceof Errors) res.status(err.code).json(err);
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,6 +2,7 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
 
 /** Restaurant  */
 routerAdmin.get("/", restaurantController.goHome);
@@ -10,7 +11,11 @@ routerAdmin
     .post("/login", restaurantController.processLogin);
 routerAdmin
 .get("/signup", restaurantController.getSignup)
-.post("/signup", restaurantController.processSignup);
+.post(
+    "/signup", 
+    makeUploader("members").single("memberImage"), 
+    restaurantController.processSignup
+    );
 routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
@@ -21,7 +26,9 @@ restaurantController.verifyReastaurant,
 productController.getAllProducts);
 routerAdmin.post(
     "/product/create", 
-    restaurantController.verifyReastaurant, 
+    restaurantController.verifyReastaurant,
+    // uploadProductImage.single("productImage"),
+    makeUploader("products").array("productImages", 5),
     productController.createNewProduct
 );
 routerAdmin.post(
