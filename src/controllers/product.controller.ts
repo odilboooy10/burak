7790f4e2a49ebc7ef0619e6127a0a9b3c305b640
odilboooy10 <odilkhon318@gmail.com>
2024-03-26 16:57:47 +0900

    feat: create new Product business logic by restaurant member

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";  
+import { ProductInput } from "../libs/types/product";
+import { AdminRequest } from "../libs/types/member";
 
 const productService = new ProductService();
 
 const productController: T = {};
+/** SPA */
+
+/** SSR */
+
 productController.getAllProducts = async (req: Request, res: Response) => {
     try{
         console.log("getAllProducts");
@@ -17,14 +23,33 @@ productController.getAllProducts = async (req: Request, res: Response) => {
     }
 };
 
-productController.createNewProduct = async (req: Request, res: Response) => {
+productController.createNewProduct = async (
+  req: AdminRequest, 
+  res: Response
+  ) => {
   try{
     console.log("createNewProduct");
-    res.send ("Done!");
+    if(!req.files?.length) 
+      throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+
+    const data: ProductInput = req.body;
+    data.productImages = req.files?.map((ele) => {
+      return ele.path;
+    });
+
+    await productService.createNewProduct(data);
+
+    res.send(  
+      `<script> alert("Sucessfull creation!"); window.location.replace("admin/product/all") </script>`);
+
+    
   } catch (err) {
     console.log("Error, createNewProduct:", err);
-    if(err instanceof Errors) res.status(err.code).json(err);
-    else res.status(Errors.standard.code).json(Errors.standard);
+    const message =
+    err instanceof Errors ? err.message : Message.SOMETING_WENT_WRONG;
+    res.send(  
+      `<script> alert("${message}"); window.location.replace("admin/product/all") </script>`
+      );
   }
 };
 
