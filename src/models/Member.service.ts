e98ecc4dac3097adc5bbacfbbf0bc999949c380f
odilboooy10 <odilkhon318@gmail.com>
2024-04-03 15:37:59 +0900

    fix: users page render real data

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,5 +1,10 @@
 import MemberModel from "../schema/Member.model";
-import { LoginInput, Member, MemberInput, MemberUpdateInput } from "../libs/types/member";
+import { 
+  LoginInput, 
+  Member, 
+  MemberInput, 
+  MemberUpdateInput 
+} from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
 import * as bcrypt from "bcryptjs";
