14fe8666ee91ab932a29f06172b3cb8835ae504c
odilboooy10 <odilkhon318@gmail.com>
2024-04-11 19:36:46 +0900

    feat: TASK W

--- src/train.ts
+++ src/train.ts
@@ -230,20 +230,36 @@ GraphQL API
 
 // TASK - V
 
-function countChars(s: string): { [key: string]: number } {
-  const charCount: { [key: string]: number } = {};
-  for (const char of s) {
-    if (char in charCount) {
-      charCount[char]++;
-    } else {
-      charCount[char] = 1;
-    }
+// function countChars(s: string): { [key: string]: number } {
+//   const charCount: { [key: string]: number } = {};
+//   for (const char of s) {
+//     if (char in charCount) {
+//       charCount[char]++;
+//     } else {
+//       charCount[char] = 1;
+//     }
+//   }
+//   return charCount;
+// }
+
+// const charCount = countChars("Mexanizatsiyalashtirilmaganligidandirda");
+// console.log("Mexanizatsiyalashtirilmaganligidandirda:" ,charCount); 
+
+
+// Task - W
+
+function chunkArray(arr: number[], chunkSize: number): number[][] {
+  const chunkedArray: number[][] = [];
+
+  for(let i = 0; i < arr.length; i += chunkSize) {
+     const chunk = arr.slice(i, i + chunkSize);
+     chunkedArray.push(chunk);
   }
-  return charCount;
+
+  return chunkedArray
 }
 
-const charCount = countChars("Mexanizatsiyalashtirilmaganligidandirda");
-console.log("Mexanizatsiyalashtirilmaganligidandirda:" ,charCount); 
+console.log("test:", chunkArray([1,2,3,4,5,6,7,8,9,10,11,12], 4));
 
 
 
