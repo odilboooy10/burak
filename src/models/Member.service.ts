b4d438edf5725bb8be333a30cf57dd007fc6ca86
odilboooy10 <odilkhon318@gmail.com>
2024-04-11 19:32:04 +0900

    fix: modify getMemberDetail business logic

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -60,6 +60,17 @@ class MemberService {
 
     }
 
+    public async getMemberDetail(member: Member): Promise<Member> {
+      const memberId = shapeIntoMongooseObjectId(member._id);
+      const result  = await this.memberModel
+      .findOne({ _id: memberId, memberStatus: MemberStatus.ACTIVE })
+      .exec();
+    if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND );  
+
+    return result;
+    }
+    
+    
     /** SSR */
     public async processSignup(input: MemberInput): Promise<Member> {
         const exist = await this.memberModel
