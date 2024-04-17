2363335bfa662eb87089a2617cc7e495c74c4e3b
odilboooy10 <odilkhon318@gmail.com>
2024-04-17 18:03:59 +0900

    feat:  fevelop getMyOrders rest Api

--- src/router.ts
+++ src/router.ts
@@ -36,9 +36,16 @@ router.get(
 );
 
 /** Order **/
-router.post("/order/create", 
+router.post(
+  "/order/create", 
 memberController.verifyAuth,
 orderController.createOrder 
 );
+router.get(
+  "/order/all",
+  memberController.verifyAuth,
+  orderController.getMyOrders
+);
+
 
 export default router;
