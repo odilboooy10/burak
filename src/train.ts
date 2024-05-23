b74d507375a84313a55c1ddf5cccf390c2961f52
odilboooy10 <odilkhon318@gmail.com>
2024-05-23 23:30:35 +0900

    feat: TASK ZO

--- src/train.ts
+++ src/train.ts
@@ -508,21 +508,21 @@ GraphQL API
 
 // Task - ZN
 
-function rotateArray(arr: number[], index: number): number[] {
-  const rotated: number[] = [];
-  const len: number = arr.length;
-  index = (index - 1) % len;
+// function rotateArray(arr: number[], index: number): number[] {
+//   const rotated: number[] = [];
+//   const len: number = arr.length;
+//   index = (index - 1) % len;
 
-  for (let i = len - index; i < len; i++) {
-    rotated.push(arr[i]);
-  }
+//   for (let i = len - index; i < len; i++) {
+//     rotated.push(arr[i]);
+//   }
 
-  for (let i = 0; i < len - index; i++) {
-    rotated.push(arr[i]);
-  }
+//   for (let i = 0; i < len - index; i++) {
+//     rotated.push(arr[i]);
+//   }
 
-  return rotated;
-}
+//   return rotated;
+// }
 
-const result: number[] = rotateArray([1, 2, 3, 4, 5, 6], 3);
-console.log(result);
+// const result: number[] = rotateArray([1, 2, 3, 4, 5, 6], 3);
+// console.log(result);
