42779e73b8d6b9657e0daed2ee2d5be1aaf99a1f
odilboooy10 <odilkhon318@gmail.com>
2024-05-26 18:19:05 +0900

    feat: Task ZQ

--- src/train.ts
+++ src/train.ts
@@ -556,28 +556,48 @@ GraphQL API
 
 // Task ZP
 
-function majorityElement(nums: number[]): number {
-  const count: { [key: number]: number } = {};
+// function majorityElement(nums: number[]): number {
+//   const count: { [key: number]: number } = {};
+
+//   for (const num of nums) {
+//     if (count[num] !== undefined) {
+//       count[num]++;
+//     } else {
+//       count[num] = 1;
+//     }
+//   }
+
+//   let majority = nums[0];
+//   let maxCount = 0;
+
+//   for (const num in count) {
+//     if (count[num] > maxCount) {
+//       maxCount = count[num];
+//       majority = Number(num);
+//     }
+//   }
+
+//   return majority;
+// }
+
+// console.log(majorityElement([1, 2, 3, 4, 5, 4, 3, 4]));
+
+// TASK ZQ
+
+function findDuplicates(nums: number[]): number[] {
+  const duplicates: number[] = [];
+  const seen: Set<number> = new Set();
+  const seenDuplicates: Set<number> = new Set();
 
   for (const num of nums) {
-    if (count[num] !== undefined) {
-      count[num]++;
-    } else {
-      count[num] = 1;
+    if (seen.has(num) && !seenDuplicates.has(num)) {
+      duplicates.push(num);
+      seenDuplicates.add(num);
     }
+    seen.add(num);
   }
 
-  let majority = nums[0];
-  let maxCount = 0;
-
-  for (const num in count) {
-    if (count[num] > maxCount) {
-      maxCount = count[num];
-      majority = Number(num);
-    }
-  }
-
-  return majority;
+  return duplicates;
 }
 
-console.log(majorityElement([1, 2, 3, 4, 5, 4, 3, 4]));
+console.log(findDuplicates([1, 2, 3, 4, 5, 4, 3, 4]));
